7ad8be9bf83904918742ba2fa285e25b009f729e
zishan <zishanenator@gmail.com>
2024-08-25 23:45:13 +0530

    validation stage

--- src/mlproject/config/configuration.py
+++ src/mlproject/config/configuration.py
@@ -1,6 +1,6 @@
 from mlproject.constants import *
 from mlproject.utils.common import read_yaml, create_directories
-from mlproject.entity.config_entity import DataIngestionConfig
+from mlproject.entity.config_entity import DataIngestionConfig, DataValidationConfig
 
 
 class ConfigurationManager:
@@ -22,7 +22,7 @@ class ConfigurationManager:
 
         create_directories([config.root_dir])
 
-        data_ingestion_confg = DataIngestionConfig(
+        data_ingestion_config = DataIngestionConfig(
             
             root_dir=config.root_dir,
             source_URL=config.source_URL,
@@ -30,4 +30,20 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
 
-        return data_ingestion_confg
+        return data_ingestion_config
+    
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir=config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
--- src/mlproject/pipeline/stage_01_data_ingestion.py
+++ src/mlproject/pipeline/stage_01_data_ingestion.py
@@ -21,11 +21,10 @@ class DataIngestionTrainingPipeline:
 
 if __name__ == "__main__":
     try:
-        config = ConfigurationManager()
-        data_ingestion_config = config.get_data_ingestion_config()
-        data_ingestion = DataIngestion(config=data_ingestion_config)
-        data_ingestion.download_file()
-        data_ingestion.extract_zip_file()
+        logger.info(f"{STAGE_NAME} started")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f"{STAGE_NAME} completed")
 
     except Exception as e:
         logger.exception(e)
